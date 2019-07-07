844a86d4b3eedd0feebfaf42ad881e61465b90ce
Tom-Challenger <it.mail.art@gmail.com>
2019-07-07 22:19:12 +0300

    React элемент

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('Hello World');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const el = (
+	<div>
+		<h1>My Todo List</h1>
+		<input placeholder="search"></input>
+		<ul>
+			<li>Learn React</li>
+			<li>Build Awesome App</li>
+		</ul>
+	</div>
+);
+//const el = React.createElement('h1', null, 'Hello World!')
+ReactDOM.render(el, document.getElementById('root'));
