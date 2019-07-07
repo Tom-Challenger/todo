7aed14c047f08ddd190500b8dc72cead003caa94
Tom-Challenger <it.mail.art@gmail.com>
2019-07-08 00:08:37 +0300

    React компоненты

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = (
-	<div>
-		<h1>My Todo List</h1>
-		<input placeholder="search"></input>
+const TodoList = () => {
+	return (
 		<ul>
 			<li>Learn React</li>
 			<li>Build Awesome App</li>
 		</ul>
-	</div>
-);
-//const el = React.createElement('h1', null, 'Hello World!')
-ReactDOM.render(el, document.getElementById('root'));
+	);
+};
+
+const AppHeader = () => {
+	return <h1>My Todo List</h1>;
+};
+
+const SearchPannel = () => {
+	return <input placeholder="search" />;
+};
+
+const App = () => {
+	return (
+		<div>
+			<AppHeader />
+			<SearchPannel />
+			<TodoList />
+		</div>
+	);
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
