8f1004a25aa312549ebbec467778f25a28908fff
Tom-Challenger <it.mail.art@gmail.com>
2019-07-10 18:40:04 +0300

    Состояния компонента

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,31 +1,80 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
+import ItemAddForm from '../item-add-form';
 
 import './app.css';
 
-const App = () => {
+export default class App extends Component {
+  constructor() {
+    super();
 
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 }
-  ];
+    this.maxId = 100;
+    this.state = {
+      todoData: [
+        { label: 'Drink Coffee', important: false, id: 1 },
+        { label: 'Make Awesome App', important: true, id: 2 },
+        { label: 'Have a lunch', important: false, id: 3 }
+      ]
+    };
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+    this.deteleItem = (id) => {
+      this.setState((state) => {
+        const idx = state.todoData.findIndex((el) => el.id === id);
+
+        // [a, b, c, d, e]
+        // [a, b,    d, e]
+        const newArray = [ 
+          ...state.todoData.slice(0, idx),
+          ...state.todoData.slice(idx + 1)
+        ];
+
+        return {
+          todoData: newArray
+        };
+      });
+    };
+
+    this.addItem = (text) => {
+      this.setState((state) => {
+        const newItem = {
+          label: text,
+          important: false,
+          id: this.maxId++
+        };
+
+        const newArray = [
+          ...state.todoData,
+          newItem
+        ];
+
+        return {
+          todoData: newArray
+        };
+      });
+    };
+  };
+
+  render() {
+    let classNames = 'todo-app';
+
+    return (
+      <div className={classNames}>
+        <AppHeader toDo={1} done={3} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+
+        <TodoList todos={this.state.todoData} 
+        onDeleted={this.deteleItem}/>
+
+        <ItemAddForm 
+        onAdd={this.addItem}/>
       </div>
-
-      <TodoList todos={todoData} />
-    </div>
-  );
+    );
+  };
 };
-
-export default App;
--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -7,35 +7,58 @@ class TodoListItem extends Component {
   constructor() {
     super();
 
-    this.onLabelClick = () => {
-      console.log(`Done ${this.props.label}`)
+    this.state = {
+      done: false,
+      important: false
     };
-  }
+
+    this.onLabelClick = () => {
+      this.setState(({done}) => {
+        return {
+          done: !done
+        };
+      });
+    };
+
+    this.onMarkImortant = () => {
+      this.setState((state) => {
+        return {
+          important: !state.important
+        };
+      });
+    };
+  };
 
   render() {
-    const {label, important = false} = this.props;
+    const {label, onDeleted} = this.props;
+    const {done, important} = this.state;
 
-    const style = {
-     color: important ? 'steelblue' : 'black',
-     fontWeight: important ? 'bold' : 'normal'
+    let classNames = 'todo-list-item';
+    if (done) {
+      classNames += ' done';
+    };
+
+    if (important) {
+      classNames += ' important';
     };
 
     return (
-      <span className="todo-list-item">
+      <span className={classNames}>
         <span
           className="todo-list-item-label"
-          style={style}
           onClick={this.onLabelClick}>
           {label}
-        </span>
+        </span> 
 
         <button type="button"
-                className="btn btn-outline-success btn-sm float-right">
+                className="btn btn-outline-success btn-sm float-right"
+                onClick={this.onMarkImortant}>
           <i className="fa fa-exclamation" />
         </button>
 
         <button type="button"
-                className="btn btn-outline-danger btn-sm float-right">
+                className="btn btn-outline-danger btn-sm float-right"
+                onClick={onDeleted}>
           <i className="fa fa-trash-o" />
         </button>
       </span>
