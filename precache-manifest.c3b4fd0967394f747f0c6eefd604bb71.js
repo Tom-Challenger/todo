6e65870ea84b063fa57186445e7ff1ce882fa30e
Tom-Challenger <it.mail.art@gmail.com>
2019-07-13 11:13:06 +0300

    Updates

---
+++ precache-manifest.c3b4fd0967394f747f0c6eefd604bb71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb769d91945ba2bf529b5368176f96a0",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "66ad07dff123227ed370",
+    "url": "/todo/static/css/main.011d6048.chunk.css"
+  },
+  {
+    "revision": "7f4da7e0c3b59296ef26",
+    "url": "/todo/static/js/2.eba5f881.chunk.js"
+  },
+  {
+    "revision": "66ad07dff123227ed370",
+    "url": "/todo/static/js/main.9bb92797.chunk.js"
+  },
+  {
+    "revision": "3ce2d46cb5510c372f4d",
+    "url": "/todo/static/js/runtime~main.7a2dc5ec.js"
+  }
+]);
